49279444a7a3bba25deb3fd20d60a3ede82ee046
AksharaBhanu <javagalbhanu@gmail.com>
2022-12-21 08:27:06 +0530

    Added Extent test for reporting

--- src/test/java/page/EnterTimeTrackPage.java
+++ src/test/java/page/EnterTimeTrackPage.java
@@ -7,22 +7,29 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class EnterTimeTrackPage {
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
+public class EnterTimeTrackPage {
+	private ExtentTest test;
+	
 	@FindBy(id="logoutLink")
 	private WebElement logoutLink;
 	
-	public EnterTimeTrackPage(WebDriver driver) {
+	public EnterTimeTrackPage(WebDriver driver,ExtentTest test) {
 		PageFactory.initElements(driver, this);
+		this.test=test;
 	}
 	
 	public boolean verifyHomePageIsDisplayed(WebDriverWait wait) {
 		
 		try{
 			wait.until(ExpectedConditions.visibilityOf(logoutLink));
+			test.log(LogStatus.PASS,"Home Page is Displayed");
 			return true;
 		}
 		catch (Exception e) {
+			test.log(LogStatus.FAIL,"Home Page is not Displayed");
 			e.printStackTrace();
 			return false;
 		}
